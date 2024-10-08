9ff254f736498024b56ae1d39665dfc25397f784
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-10-08 15:35:29 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,19 @@
 function redirecionar(id) {
     let id_a = id
     let a = document.getElementById(id_a);
-    a.setAttribute('href', '/' + id_a + '.html');
+    a.setAttribute('href', './' + id_a + '.html');
 }
+
+function redirecionarNav(id) {
+    let id_a = id
+    let a = document.querySelector('.' + id_a);
+    console.log(id_a)
+    a.setAttribute('href', './' + id_a + '.html');
+}
+
+let menuIcon = document.querySelector('#menu-icon');
+const navbar = document.getElementById('navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+}   
